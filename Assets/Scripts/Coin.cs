79168e3bbf56020c6db0ac8d4478a8bd5a2facc0
Nika <144957020+Pirana07@users.noreply.github.com>
2025-12-18 16:49:16 +0400

    +MenuButtons

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Coin", menuName = "Coin", order = 0)]
 public class Coin : ScriptableObject {
 
+    [Header("Description")]
     public new string name;
     public string desc;
-
     public Sprite artWork;
 
+    [Header("MoneyRelated")]
     public int Income;
     public int Cost;
     
+    [Header("Rarity")]
+    public string rarity;
+    public string chances;
 
 }
