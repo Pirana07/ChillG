512551ebc9ec147b82508555ce5360bff53f1294
Nika <144957020+Pirana07@users.noreply.github.com>
2025-12-19 22:11:47 +0400

    +new font, + new Interior, +bug fixes

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -2,25 +2,14 @@ using UnityEngine;
 
 public class MenuButtons : MonoBehaviour
 {
-    int i = 0;
-    bool isItActive;
     [SerializeField] GameObject[] menuObject;
 
     public void OnClickMenu(int menuIndex)
     {
-        // 1 or 0 and turns on or off menu with index 
-        i++;
-        
-        if(i == 1 && isItActive == false)
-            isItActive = true;
-        else if(i == 2 && isItActive == true || isItActive == true)
-            isItActive = false;
-        else{
-            i = 0;
-            isItActive = true;
+        //fixed buges and much better code:
+        for (int i = 0; i < menuObject.Length; i++)
+        {
+            menuObject[i].SetActive(i == menuIndex && !menuObject[i].activeSelf);
         }
-    
-        menuObject[menuIndex].SetActive(isItActive);
-        // Debug.Log(i);
     }
 }
